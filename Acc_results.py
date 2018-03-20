41388f5e3c512bcc9f133c8f9e6edbc4a30022f3
Raquel <raquel2terrero@gmail.com>
2018-03-20 17:29:36 +0100

    test para request, results para mostrar gráficas

---
+++ Acc_results.py
@@ -0,0 +1,145 @@
+## --- COMPARING THE SERVICES --- ##
+""" This script opens the file where the data is stored and show it 
+    in form of graphs to get a conclusion of which service works best 
+    in a quantitative way """
+
+# -- IMPORTED LIBRARIES -- # 
+import io
+import os
+import pickle
+import csv
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# -- FUNCTION DEFINITIONS -- #
+
+def get_results():
+    """ 
+    This function loads the data saved in the "pkl" file and returns it. 
+    """
+    file = open('results.pkl','rb')
+    return pickle.load(file)
+
+def showing_data(results):
+    """
+    This function reorganizes the data and plots the graphs  
+    """
+    # -- PLOTTING POSITION -- #
+    fig1 = plt.figure(figsize=(12,8))
+    plt.hist((results['pos_google'],results['pos_amazon']),bins=np.arange(13)-0.5,\
+            label=("Google Positions","Amazon Positions"),density=False,alpha=1,color=('royalblue','darkorange'))
+    plt.title("Histogram of the Frequency based on Positions ")
+    plt.xlabel("Positions")
+    plt.ylabel("Frequency")
+    plt.legend()
+    plt.grid(True)
+    plt.xticks(range(12))
+    plt.axis([0.25, 11.75,0,170])
+    plt.savefig('Out/Positions.png',bbox_inches='tight')
+    plt.close(fig1)
+
+    # -- SCORE OF THE FIRST POSITION -- #
+    samples = len(results['pos_google'])
+    ind_google = [i for i in range(samples) if results['pos_google'][i] == 1]
+    ind_amazon = [i for i in range(samples) if results['pos_amazon'][i] == 1]
+    scr_google =[ results['score_google'][i] for i in ind_google]
+    scr_amazon =[ results['score_amazon'][i] for i in ind_amazon]
+    spl_google = len(scr_google)
+    spl_amazon = len(scr_amazon)
+    avg_scr_google = sum(scr_google)/float(spl_google)
+    avg_scr_amazon = sum(scr_amazon)/float(spl_amazon)
+    fig2 = plt.figure(figsize=(12,8))
+    plt.plot(range(spl_google),scr_google,'-',linewidth=0.9,label='Google Score',color='royalblue')
+    plt.axhline(y=avg_scr_google,xmin=0,xmax=spl_google,linewidth=2,\
+                label='Average Google Score',color='navy',alpha=1)
+    plt.plot(range(spl_amazon),scr_amazon,'-',linewidth=0.9,label='Amazon Score',color='darkorange')
+    plt.axhline(y=avg_scr_amazon,xmin=0,xmax=spl_amazon,linewidth=2,\
+                label='Average Amazon Score',color='orangered',alpha=1)
+    plt.title('Score of the first Positions')
+    plt.xlabel('Requests')
+    plt.ylabel('Confidence')
+    plt.legend(loc='lower right')
+    plt.grid(True)
+    plt.axis([0,max(spl_google,spl_amazon),0.5,1])
+    plt.savefig('Out/First_Score.png',bbox_inches='tight')
+    plt.close(fig2)
+
+    # -- SCORE OF THE DETECTED POSITIONS -- #
+    indten_google = [i for i in range(samples) if results['pos_google'][i] <= 10]
+    indten_amazon = [i for i in range(samples) if results['pos_amazon'][i] <= 10]
+    scrten_google =[ results['score_google'][i] for i in indten_google]
+    scrten_amazon =[ results['score_amazon'][i] for i in indten_amazon]
+    splten_google = len(scrten_google)
+    splten_amazon = len(scrten_amazon)
+    avgten_scr_google = sum(scrten_google)/float(splten_google)
+    avgten_scr_amazon = sum(scrten_amazon)/float(splten_amazon)
+    fig3 = plt.figure(figsize=(12,8))
+    plt.plot(range(splten_google),scrten_google,'-',linewidth=0.9,label='Google Score',color='royalblue')
+    plt.axhline(y=avgten_scr_google,xmin=0,xmax=splten_google,linewidth=2,\
+                label='Average Google Score',color='navy',alpha=1)
+    plt.plot(range(splten_amazon),scrten_amazon,'-',linewidth=0.9,label='Amazon Score',color='darkorange')
+    plt.axhline(y=avgten_scr_amazon,xmin=0,xmax=splten_amazon,linewidth=2,\
+                label='Average Amazon Score',color='orangered',alpha=1)
+    plt.title('Score of the first 10 Positions')
+    plt.xlabel('Requests')
+    plt.ylabel('Confidence')
+    plt.legend(loc='lower right')
+    plt.grid(True)
+    plt.axis([0,max(splten_google,splten_amazon),0.5,1])
+    plt.savefig('Out/Detected_Scores.png',bbox_inches='tight')
+    plt.close(fig3)
+
+    # -- PRINTING PUNTUATION -- #
+    with open('Out/Results.txt','w') as text_file:
+        text_file.write('_-_-_-_-_-_-_-_-_-_-_-_-_ ACCURACY RESULTS _-_-_-_-_-_-_-_-_-_-_-_-_\n')
+        text_file.write('(1) General Data:'+'\n')
+        text_file.write('-- Total Requests: {}.\n'.format(samples))
+        text_file.write('-- Google Detections: {}.\n'.format(splten_google))
+        text_file.write('-- Amazon Detections: {}.\n'.format(splten_amazon))
+        msg = (sum(results['score_google'])/float(samples))*100
+        text_file.write('(2) Total Accuracy:\n')
+        text_file.write('-- Total Google Accuracy: {0:.2f}%.\n'.format(msg))
+        msg = (sum(results['score_amazon'])/float(samples))*100
+        text_file.write('-- Total Amazon Accuracy: {0:.2f}%.\n'.format(msg))
+        text_file.write('(3) Accuracy of the first Positions:\n')
+        text_file.write('-- First Positions Google Accuracy: {0:.2f}%.\n'.format(avg_scr_google*100))
+        text_file.write('-- First Positions Amazon Accuracy: {0:.2f}%.\n'.format(avg_scr_amazon*100))
+        text_file.write('(4) Accuracy of the detected requests:\n')
+        text_file.write('-- Detected Google Accuracy: {0:.2f}%.\n'.format(avgten_scr_google*100))
+        text_file.write('-- Detected Amazon Accuracy: {0:.2f}%.\n'.format(avgten_scr_amazon*100))
+    text_file.close()
+
+def write_csv(results):
+    """
+    This function writes the dictionary results in a csv file.
+    """
+    punt_google = ['punt_google',results['punt_google']]
+    punt_amazon = ['punt_amazon',results['punt_amazon']]
+    results.pop('punt_google',None)
+    results.pop('punt_amazon',None)
+    keys = sorted(results.keys())
+    with open('Out/Results.csv','w') as csv_file:
+        writer = csv.writer(csv_file,delimiter = ",")
+        writer.writerow(punt_google)
+        writer.writerow(punt_amazon)
+        writer.writerow(keys)
+        writer.writerows(zip(*[results[key] for key in keys]))
+
+def main():
+    """ 
+    This main program gets the results from a pickle file, and shows the
+    data in form of graphs. Also the results obtained are writen in a csv file
+    if the user wants to get more detailed results.
+    """ 
+    # -- GET THE RESULTS FROM A PICKLE FILE -- #
+    results = get_results()
+
+    # -- PLOTTING THE RESULTS OBTAINED -- #
+    showing_data(results)
+
+    # -- WRITING THE RESULTS IN A CSV FILE -- #
+    write_csv(results)
+
+if __name__ == '__main__':
+    main()
---
+++ Acc_test.py
@@ -0,0 +1,139 @@
+## --- ACCURACY TEST --- ##
+""" 
+This script moves through the dataset of ingredients 
+and uses the google vision and amazon rekognition service to obtain the labels.
+Compare the two services and gives a file with all the data recolected.
+"""
+
+# -- IMPORTED LIBRARIES -- # 
+import io
+import os
+import boto3
+import pickle
+import random
+
+from pathlib import Path
+from google.cloud import vision
+from google.cloud.vision import types
+
+# -- DEFINING THE RESULTS DICTIONARY -- #
+results = {'punt_google':0,
+        'punt_amazon':0,
+        'file_name':[],
+        'pos_google':[],
+        'pos_amazon':[],
+        'score_google':[],
+        'score_amazon':[]
+        }
+google_words = {}
+amazon_words = {}
+
+# -- FUNCTION DEFINITIONS -- #
+
+def google_accuracy(vision_client,image,real_label):
+    """ 
+    This function calculates the accuracy given by the google vision service
+        by one image. We valorate relevant information. 
+    """
+    keys = google_words.keys()
+    response = vision_client.label_detection(image=image)
+    labels = response.label_annotations
+    counter = 0
+    pos=11
+    score=0.0
+    for label in labels:
+        counter += 1
+        if real_label == label.description:
+            pos = counter
+            score=(11-counter)/10
+        else:
+            if label in keys:
+                google_words[label] += 1
+            else:
+                google_words[label] = 1
+    return score, pos
+
+def amazon_accuracy(rekognition_client,content,real_label):
+    """ 
+    This function calculates the accuracy given by the amazon rekognition service
+        by one image. We valorate relevant information. 
+    """
+    keys = amazon_words.keys()
+    response = rekognition_client.detect_labels(Image={'Bytes': content},MaxLabels=10)
+    labels = response['Labels']
+    counter = 0
+    pos=11
+    score=0.0
+    for label in labels:
+        counter += 1
+        if real_label == label['Name'].lower():
+            pos = counter
+            score=(11-counter)/10
+        else:
+            if label in keys:
+                amazon_words[label] += 1
+            else:
+                amazon_words[label] = 1
+    return score, pos
+
+def obtaining_results(data_directory,vision_client,rekognition_client):
+    """ 
+    This function moves through directories in the DataSet and gets the position
+        and score of both (google & amazon) services from the images of the DataSet. 
+    """
+    for path in data_directory.iterdir():
+        if path.is_dir():
+            for ingredient in path.glob('*.jpg'):
+                file_name = str(path.joinpath(ingredient.name))
+                real_label = ingredient.name[:-7]       
+                with io.open(file_name,'rb') as image_file:
+                    content = image_file.read()
+                    image = types.Image(content=content)
+                # -- CALLING GOOGLE & AMAZON SERVICES -- #
+                s_google,p_google = google_accuracy(vision_client,image,real_label)
+                s_amazon,p_amazon = amazon_accuracy(rekognition_client,content,real_label)
+                print(file_name)
+
+                # -- OBTAINING RESULTS FOR ONE IMAGE -- #
+                results['file_name'].append(ingredient.name[:-4])
+                if s_google > s_amazon:
+                    results['punt_google']+=1
+                elif s_google < s_amazon:
+                    results['punt_amazon']+=1
+                results['pos_google'].append(p_google)
+                results['pos_amazon'].append(p_amazon)
+                results['score_google'].append(s_google)
+                results['score_amazon'].append(s_amazon)
+                
+def main():
+    """ 
+    The main program gets the images from the dataset
+        and evaluates how the google vision services operates. 
+    """
+    # -- SETTING THE DIRECTORIES -- #
+    data_directory = Path.cwd() / 'DataSet'
+    key_path = str(Path.cwd())+"/apikey.json"
+
+    # -- GOOGLE APPLICATION CREDENTIALS -- #
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
+
+    # -- SERVICES CLIENTS -- #
+    vision_client = vision.ImageAnnotatorClient()
+    rekognition_client = boto3.client('rekognition','eu-west-1')
+
+    # -- ACCESING TO THE DATASET IMAGES -- #
+    obtaining_results(data_directory,vision_client,rekognition_client)
+
+    # -- SAVING VARIABLE RESULTS IN A PICKLE -- #
+    with open('results.pkl','wb') as file:
+        pickle.dump(results,file)
+    file.close()
+    with open('google_words.pkl','wb') as file:
+        pickle.dump(google_words,file)
+    file.close()
+    with open('amazon_words.pkl','wb') as file:
+        pickle.dump(amazon_words,file)
+    file.close()
+    
+if __name__ == '__main__':
+    main()
